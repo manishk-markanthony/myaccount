8ffd6e9ab68d245d5c63c066b48125e574f4abf3
Manish Kumar <manish.kumar@markanthony.com>
2025-02-14 20:48:53 +0530

    Added ping related changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,90 @@
-import Image from "next/image";
+"use client"
+import Script from "next/script";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="./next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  //*** Populate the parameters below from your DaVinci environment ***/
+  const companyId = "2422107b-7011-48bb-8366-735e927271f2";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="./vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="./file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="./window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="./globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const skApiKey =
+    "67325a24e28d69f37f5c49a66cfe4771d5ad368e0a7641c20b099418f69fcc28bfaf7bd2e0d2c0ef5f0eb42e722efd2354b2b4d6ef67c1f5426dc39ba3af8180b5e8ef2007e596822ce0d31a037baa0b674c7468e7a1ed7628cfdfecb8ecce5db58597dbab62944c0f431ef8fe94797f9968f0980678bd610973d25df2e34c13";
+
+  //*** Build the get SK Token URL. ***/
+  const skGetTokenUrl =
+    `https://orchestrate-api.pingone.com/v1/company/${companyId}/sdktoken`;
+
+
+  //*** Add the API Key from your DaVinci application. ***/
+  var headers = new Headers();
+  headers.append("X-SK-API-KEY", skApiKey);
+
+  const requestOptions = {
+    method: "GET",
+    headers: headers,
+    redirect: "follow",
+  };
+
+  let queryString = "";
+  //queryString
+  console.log(`queryString = ${queryString}`);
+
+  useEffect(() => {
+    fetch(skGetTokenUrl, requestOptions).then((response) => response.json)
+      .then((responseData) => {
+        const props = {
+          config: {
+            method: "runFlow",
+            apiRoot: "https://auth.pingone.com/",
+            accessToken: responseData.access_token,
+            companyId: companyId,
+            policyId: "eaaacbd2b5dc514e3bedb6e1b3e4e571",
+            parameters: {
+              "redirectUrl": queryString,
+              "brand": "WCB",
+              "country": "US"
+            }
+          },
+          useModal: false,
+          successCallback,
+          errorCallback,
+          onCloseModal,
+        };
+        setTimeout(function () {
+          document.getElementById("loader").style.display = 'none';
+        }, 1500);
+
+        davinci.skRenderScreen(
+          document.getElementsByClassName("skWidget")[0],
+          props
+        );
+      })
+    function successCallback(response) {
+      console.log(response, "additional properties");
+      let multipassURL = response?.additionalProperties?.multipassURL;
+      setTimeout(() => {
+        window.location.href = multipassURL;
+      }, 100);
+
+    }
+
+    function errorCallback(error) {
+      console.log('error while running flow ', error);
+      if (error && error.httpResponseCode === 401 && error.errorMessage === "Unauthorized!") {
+        window.location.reload();
+      }
+    }
+
+    function onCloseModal() {
+      console.log("onCloseModal");
+    }
+  })
+
+  return (
+    <div id="ping-container"> 
+      <h2>ping container</h2>
+      <Script type="text/javascript" src="https://assets.pingone.com/davinci/latest/davinci.js" />
+      <div className="loader" id="loader"></div>
+      <div id="widget" className="skWidget"></div>
     </div>
   );
 }

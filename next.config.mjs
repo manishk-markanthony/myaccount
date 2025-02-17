5417f9413372544dac0039c2ebe6b9a2f8fb0470
Manish Kumar <manish.kumar@markanthony.com>
2025-02-17 17:15:58 +0530

    Added consts

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output : "standalone",
     publicRuntimeConfig: {
         companyId :"2422107b-7011-48bb-8366-735e927271f2",
         skApiKey :"67325a24e28d69f37f5c49a66cfe4771d5ad368e0a7641c20b099418f69fcc28bfaf7bd2e0d2c0ef5f0eb42e722efd2354b2b4d6ef67c1f5426dc39ba3af8180b5e8ef2007e596822ce0d31a037baa0b674c7468e7a1ed7628cfdfecb8ecce5db58597dbab62944c0f431ef8fe94797f9968f0980678bd610973d25df2e34c13",
         apiRoot: "https://auth.pingone.com/",
         method: "runFlow",
         orchestrateApiUrl :"https://orchestrate-api.pingone.com"
-    }
+    },
+    output : "standalone",
 };
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,24 @@
 "use client"
 import Script from "next/script";
 import { useEffect } from "react";
-const { publicRuntimeConfig } = getConfig()
-
 
 
 export default function Home(props) {
   console.log(props.session);
   useEffect(() => {
     //*** Populate the parameters below from your DaVinci environment ***/
-    //const companyId = "2422107b-7011-48bb-8366-735e927271f2";
+    const companyId = "2422107b-7011-48bb-8366-735e927271f2";
 
-    //const skApiKey =
-      //"67325a24e28d69f37f5c49a66cfe4771d5ad368e0a7641c20b099418f69fcc28bfaf7bd2e0d2c0ef5f0eb42e722efd2354b2b4d6ef67c1f5426dc39ba3af8180b5e8ef2007e596822ce0d31a037baa0b674c7468e7a1ed7628cfdfecb8ecce5db58597dbab62944c0f431ef8fe94797f9968f0980678bd610973d25df2e34c13";
+    const skApiKey =
+      "67325a24e28d69f37f5c49a66cfe4771d5ad368e0a7641c20b099418f69fcc28bfaf7bd2e0d2c0ef5f0eb42e722efd2354b2b4d6ef67c1f5426dc39ba3af8180b5e8ef2007e596822ce0d31a037baa0b674c7468e7a1ed7628cfdfecb8ecce5db58597dbab62944c0f431ef8fe94797f9968f0980678bd610973d25df2e34c13";
 
     //*** Build the get SK Token URL. ***/
     const skGetTokenUrl =
-      `${publicRuntimeConfig.orchestrateApiUrl}/v1/company/${publicRuntimeConfig.companyId}/sdktoken`;
+      `https://orchestrate-api.pingone.com/v1/company/${companyId}/sdktoken`;
 
     //*** Add the API Key from your DaVinci application. ***/
     var headers = new Headers();
-    headers.append("X-SK-API-KEY", publicRuntimeConfig.skApiKey);
+    headers.append("X-SK-API-KEY", skApiKey);
 
     const requestOptions = {
       method: "GET",

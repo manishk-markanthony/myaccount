8b4d7431ef7a13c01f5a645d645e8081b4873905
Manish Kumar <manish.kumar@markanthony.com>
2025-03-11 15:19:17 +0530

    Loading indicator

--- src/app/my-account/page.js
+++ src/app/my-account/page.js
@@ -1,18 +1,34 @@
 "use client"
 import MyAccount from './my-account';
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 import Login from '../login/login';
 import { getCookie } from '@/util/helper';
 
 export default function Page() {
-      const [isLoggedIn, setIsLoggedIn] = useState(false);
-      useEffect(() => {
-        setIsLoggedIn(getCookie("login"));
-      }, [isLoggedIn]);
-      console.log(`isLoggedIn : ${isLoggedIn}`);
-      if(!isLoggedIn) { 
-        return <Login></Login>
-      }
-    return <MyAccount/>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    setIsLoggedIn(getCookie("login"));
+  }, [isLoggedIn]);
+  console.log(`isLoggedIn : ${isLoggedIn}`);
+  if (!isLoggedIn) {
+
+    return (
+      <>
+        <Suspense fallback={<Loading />}>
+          <Login />
+        </Suspense>
+      </>
+    )
+  }
+  return (
+    <>
+      <Suspense fallback={<Loading />}>
+        <MyAccount />
+      </Suspense>
+    </>
+  );
+}
+function Loading() {
+  return <h2>🌀 Loading...</h2>;
 }

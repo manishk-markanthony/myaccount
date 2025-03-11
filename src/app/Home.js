b39ce7fe69568764a0ecbc6a35da76fa59bae0ef
Manish Kumar <manish.kumar@markanthony.com>
2025-03-11 13:52:22 +0530

    Updated text on home screen

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,7 +1,13 @@
 export default function Home(props) {
-    return (
-        <div id="ping-container"> 
-        <h1> White claw UK : My Account</h1>
-        </div>
-      );
+  return (
+    <div id="ping-container">
+      <h1> White claw UK</h1>
+      <h3>Please login to continue</h3>
+
+      <div>
+      <a href="/login">Login</a> <br/>
+      <a href="/create-account">Create account</a>
+      </div>
+    </div>
+  );
 }

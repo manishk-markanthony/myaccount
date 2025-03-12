170587a485e83e4ca102b5b24a3658a0868746da
Manish Kumar <manish.kumar@markanthony.com>
2025-03-12 19:08:22 +0530

    Added ping widget back

--- src/app/my-account/my-account.js
+++ src/app/my-account/my-account.js
@@ -1,9 +1,96 @@
+import { API_ROOT, BRAND_WCB, COMPANY_ID, COUNTRY_US, METHOD_GET, MY_ACCOUNT_POLICY_ID, PING_RUN_FLOW, SK_API_KEY, SK_GET_TOKEN_URL } from "@/util/constant";
+import Script from "next/script";
+
 export default function MyAccount(props) {
+    useEffect(() => {
+
+        var headers = new Headers();
+        headers.append("X-SK-API-KEY", SK_API_KEY);
+
+        const requestOptions = {
+            method: METHOD_GET,
+            headers: headers,
+            redirect: "follow",
+        };
+
+        //*** Get URL string parameters***/
+        let queryString = window.location.search.trim().length > 0 ? decodeURIComponent(window.location.search) : "";
+
+        //*** Ensure if query string has return url if not redirect back to account ***/
+        queryString = queryString.indexOf("/") >= 0 ? queryString.substr(queryString.indexOf("=") + 1) : "/account";
+
+        //queryString
+        console.log(`queryString = ${queryString}`);
+
+
+        //*** Retrieve SK Token ***/
+        fetch(SK_GET_TOKEN_URL, requestOptions)
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log(`responseData : ${responseData}`);
+                var props = {
+                    config: {
+                        method: PING_RUN_FLOW,
+                        apiRoot: API_ROOT,
+                        accessToken: responseData.access_token,
+                        companyId: COMPANY_ID,
+                        policyId: MY_ACCOUNT_POLICY_ID,
+                        parameters: {
+                            "redirectUrl": queryString,
+                            "brand": BRAND_WCB,
+                            "country": COUNTRY_US
+                        }
+                    },
+                    useModal: false,
+                    successCallback,
+                    errorCallback,
+                    onCloseModal,
+                };
+                /*** Invoke the Widget ****/
+                // console.log(props);
+
+
+                setTimeout(function () {
+                    document.getElementById("loader").style.display = 'none';
+                }, 1500);
+
+                davinci.skRenderScreen(
+                    document.getElementsByClassName("skWidget")[0],
+                    props
+                )
+            })
+            .catch((error) => {
+                console.log('error while running flow ', error);
+                if (error && error.errorMessage === "Unauthorized!" || (error && error.httpResponseCode === 401)) {
+                    window.location.reload();
+                }
+            });
+
+        function successCallback(response) {
+            console.log(response, "additional properties");
+            let multipassURL = response?.additionalProperties?.multipassURL;
+            setTimeout(() => {
+                window.location.href = multipassURL;
+            }, 100);
+
+        }
+
+        function errorCallback(error) {
+            console.log('error while running flow ', error);
+            if (error && error.httpResponseCode === 401 && error.errorMessage === "Unauthorized!") {
+                window.location.reload();
+            }
+        }
+
+        function onCloseModal() {
+            console.log("onCloseModal");
+        }
+    })
     return (
-        <div>
-            <h1>My Account :: Dashboard </h1>
-            <br />
-            <h2>Hello World</h2>
+        <div id="ping-container">
+            <Script type="text/javascript" src="https://assets.pingone.com/davinci/latest/davinci.js" />
+            <div className="loader" id="loader"></div>
+            <div id="widget" className="skWidget"></div>
         </div>
     );
 }
